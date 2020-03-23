a6a3675a5b0074812edc57fdddbf8183b6ae2297
Hushunkang <2752962035@qq.com>
2020-03-24 06:49:53 +0800

    spring framework bean之间的关系之继承和依赖

---
+++ spring1/src/com/atguigu/spring/beans/relation/Address.java
@@ -0,0 +1,50 @@
+package com.atguigu.spring.beans.relation;
+
+import com.atguigu.spring.beans.autowire.Person;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author hskBeginner Email：2752962035@qq.com
+ * @version 1.0
+ * @description
+ * @create 2020年03月24日 05时35分44秒
+ */
+public class Address {
+
+    private String city;
+    private String street;
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                '}';
+    }
+
+}
+
+class AddressTest {
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans-relation.xml");
+        Address address = (Address) ctx.getBean("address");
+        System.out.println(address);
+        address = (Address) ctx.getBean("address2");
+        System.out.println(address);
+//        address = (Address) ctx.getBean("address3");//不能获取到抽象的bean
+//        System.out.println(address);
+        Person person = (Person) ctx.getBean("person");
+        System.out.println(person);
+    }
+
+}
