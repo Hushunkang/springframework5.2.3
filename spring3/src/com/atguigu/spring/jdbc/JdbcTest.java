668fbcb22dd60032405ef3b785fe856b7e95f0a6
Hushunkang <2752962035@qq.com>
2020-03-25 05:55:58 +0800

    spring framework 使用NamedParameterJdbcTemplate可以为jdbc参数传递名称

--- spring3/src/com/atguigu/spring/jdbc/JdbcTest.java
+++ spring3/src/com/atguigu/spring/jdbc/JdbcTest.java
@@ -5,8 +5,13 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author hskBeginner Email：2752962035@qq.com
@@ -18,10 +23,14 @@ public class JdbcTest {
 
     private ApplicationContext ctx;
     private JdbcTemplate jdbcTemplate;
+    private DepartmentDao departmentDao;
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     {
         ctx = new ClassPathXmlApplicationContext("spring-config.xml");
         jdbcTemplate = (JdbcTemplate) ctx.getBean("jdbcTemplate");
+        departmentDao = (DepartmentDao) ctx.getBean("departmentDao");
+        namedParameterJdbcTemplate = (NamedParameterJdbcTemplate) ctx.getBean("namedParameterJdbcTemplate");
     }
 
     @Test
@@ -43,6 +52,32 @@ public class JdbcTest {
     @Test
     public void testBatchUpdate(){
         //...spring JdbcTemplate的例子具体可看看ppt课件
+    }
+
+    //1、优点：使用NamedParameterJdbcTemplate可以为jdbc参数传递名称
+    //2、缺点：麻烦一些
+    @Test
+    public void testNamedParameterJdbcTemplate(){
+        String sql = "insert into departments(department_id,department_name,manager_id,location_id) values(:id,:deptName,:mid,:lid)";
+        Map<String,Object> paramMap = new HashMap<>();
+        paramMap.put("id",280);
+        paramMap.put("deptName",280);
+        paramMap.put("mid",115);
+        paramMap.put("lid",1700);
+        namedParameterJdbcTemplate.update(sql,paramMap);
+    }
+
+    @Test
+    public void testNamedParameterJdbcTemplate2(){
+        String sql = "insert into departments(department_id,department_name,manager_id,location_id) values(:id,:departmentName,:managerId,:locationId)";
+        Department department = new Department();
+        department.setId(290);
+        department.setDepartmentName("290");
+        department.setManagerId(115);
+        department.setLocationId(1700);
+
+        SqlParameterSource sqlParameterSource = new BeanPropertySqlParameterSource(department);
+        namedParameterJdbcTemplate.update(sql,sqlParameterSource);
     }
 
 }
